fec2ae54f147c7923f70a35d1f21ab2953919ebb
Zixuan Zou <zouzixuan1117@gmail.com>
2021-10-19 17:27:11 -0400

    add scatterplot for genres

--- sales_ratings.py
+++ sales_ratings.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
 
-st.title('Video Game Sales and Ratings')
+st.title('Video Game Sales')
 
 df = pd.read_csv('Video_Games_Sales_as_at_22_Dec_2016.csv')
 
@@ -15,7 +15,8 @@ st.write(
 The 3D scatterplot below visualizes the distribution of sales of games with release date from 1985 to 2016. 
 Each dot in the 3D space represents a game, and the x, y and z axis represent its sales in NA, EU and JP.
 The color of the dot represents the year of release of the game.
-''')
+'''
+)
 fig = px.scatter_3d(
                 data_frame=df,
                 x="NA_Sales",
@@ -29,11 +30,20 @@ st.plotly_chart(fig, use_container_width=True)
 
 # bar chart of sales over years
 st.subheader("Sales of over years")
+st.write(
+'''
+The bar chart below shows the average and sum of video game sales over years.
+'''
+)
 
 # clean up the dataset
 df = df.loc[df['Year_of_Release'] <= 2016]
 
-selected_region = st.selectbox('Sales by Region', ('NA_Sales', 'EU_Sales', 'JP_Sales', 'Global_Sales'), key=0)
+histo_type = st.selectbox('Histogram Function', ('Average', 'Sum'), key=0)
+st.write('Selected Histogram Function: ', histo_type)
+func = "avg" if (histo_type == "Average") else "sum"
+
+selected_region = st.selectbox('Sales by Region', ('NA_Sales', 'EU_Sales', 'JP_Sales', 'Global_Sales'), key=1)
 st.write('Selected Sale Region: ', selected_region)
 
 fig = px.histogram(
@@ -41,14 +51,12 @@ fig = px.histogram(
     x="Year_of_Release",
     y=selected_region,
     title="Sum of Sales by Years",
-    histfunc="sum",
+    histfunc=func,
 )
 
 st.plotly_chart(fig, use_container_width=True)
 
-
-
-# scatterplot of ratings vs. sales
+# 2D scatterplot of ratings vs. sales
 st.subheader("scatterplot of ratings vs. sales")
 st.write(
 '''
@@ -67,11 +75,11 @@ st.write("Sales and ratings in: ", year)
 score_type = st.selectbox('Score Type:', ('Critic_Score', 'User_Score'))
 st.write('Selected Score Type: ', score_type)
 
-sale_region = st.selectbox('Sales by Region', ('NA_Sales', 'EU_Sales', 'JP_Sales', 'Global_Sales'), key=1)
+sale_region = st.selectbox('Sales by Region', ('NA_Sales', 'EU_Sales', 'JP_Sales', 'Global_Sales'), key=2)
 st.write('Selected Sale Region: ', sale_region)
 
 platforms = df['Platform'].unique().tolist()
-platform = st.multiselect('Platform(s): ', platforms, ['PS4', 'XOne'])
+platform = st.multiselect('Platform(s): ', platforms, ['PS4', 'XOne'], key=0)
 st.write('Selected Platform(s): ', platform)
 
 cur_dataset =  df.loc[df['Year_of_Release'] == year]
@@ -89,5 +97,29 @@ fig = px.scatter(
 
 st.plotly_chart(fig, use_container_width=True)
 
-st.subheader("Genere vs. Sales")
+# 2D scatterplot of sales vs. year of release (different genres)
+st.subheader("Genre vs. Sales")
+st.write(
+'''
+This 2D scatterplot visualizes sales of games of different genres over years.
+'''
+)
+
+sale_region = st.selectbox('Sales by Region', ('NA_Sales', 'EU_Sales', 'JP_Sales', 'Global_Sales'), key=3)
+st.write('Selected Sale Region: ', sale_region)
+
+genres = df['Genre'].unique().tolist()
+genre = st.multiselect('Genre(s): ', genres, ['Action', 'Shooter'], key=1)
+st.write('Selected Genre(s): ', genre)
+
+df_genre = df.loc[df['Genre'].isin(genre)]
+fig = px.scatter(
+    data_frame=df_genre,
+    x="Year_of_Release",
+    y=sale_region,
+    color="Genre",
+    title="Game Sales and Genres",
+)
+st.plotly_chart(fig, use_container_width=True)
+
 
